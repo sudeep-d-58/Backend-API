c28478ad1eeb08efe73414a563da5882a4ee2dda
sudeep-d-58 <Sudeep@PwC123>
2023-11-07 21:38:45 +0530

    First Commit

---
+++ src/test/java/com/order/service/BackendOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
